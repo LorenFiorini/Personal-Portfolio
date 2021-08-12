9b77acad73cbc3a1cf41c0b93354fc0b22d18253
Lorenzo Fiorini <lorenfiorini@gmail.com>
2021-08-12 04:06:40 -0700

    Colors 🎨

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff4a148c);
+const primaryColorLight = Color(0xFF7c43bd);
+const primaryColorDark = Color(0xFF12005e);
+
+const secondaryColor = Color(0xff00acc1);
+const secondaryColorLight = Color(0xFF5ddef4);
+const secondaryColorDark = Color(0xFF007c91);
+
+
